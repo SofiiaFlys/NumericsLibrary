1db37ae011268e05cb89b1293ef10c09c6622cb4
Sofia <flyssoffia@gmail.com>
2019-10-03 14:18:35 +0300

    add new method Systema

--- TryNumericsLibrary/TryNumericsLibrary/Calculation.cs
+++ TryNumericsLibrary/TryNumericsLibrary/Calculation.cs
@@ -10,5 +10,15 @@ namespace TryNumericsLibrary
 {
     class Calculation
     {
+        public static double[] Systema(double[] x)
+        {
+            double x1 = x[0];
+            double x2 = x[1];
+            double[] res = new double[3];
+            res[0] = x1 - Math.Pow(10, 6);
+            res[1] = x2 - 2 * Math.Pow(10, -6);
+            res[2] = x1 * x2 - 2;
+            return res;
+        }
     }
 }
